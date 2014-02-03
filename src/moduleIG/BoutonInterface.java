b57241f62bbbd61ae04c0bb8f7ddfeecb113cdd1
Ramzi Zaccharie <zaccharie.ramzi@gmail.com>
2014-02-03 15:04:35 +0100

    création nouvelle méthode pour avoir une référence sur la fenêtre

--- src/moduleIG/BoutonInterface.java
+++ src/moduleIG/BoutonInterface.java
@@ -3,6 +3,7 @@ package moduleIG;
 public interface BoutonInterface {
 
 	String setText( String texteDuBouton);
+	FenetreInterface setFenetreDAppartenance(FenetreInterface FenetreDAppartenance);
 	FenetreInterface ouvertureFenetre(String typeFenetre);
 	void envoiDInformationControleur();
 	
