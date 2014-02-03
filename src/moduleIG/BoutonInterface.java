096dc5d24cc65ccde5c2d3454c76159e040b70b1
Ramzi Zaccharie <zaccharie.ramzi@gmail.com>
2014-02-03 15:29:44 +0100

    Ajout des différentes méthodes nécessaires à l'interface Bouton. A verifier avec S. Gardoll

--- src/moduleIG/BoutonInterface.java
+++ src/moduleIG/BoutonInterface.java
@@ -3,7 +3,11 @@ package moduleIG;
 public interface BoutonInterface {
 
 	String setText( String texteDuBouton);
-	FenetreInterface setFenetreDAppartenance(FenetreInterface FenetreDAppartenance);
+	FenetreInterface setFenetreDAppartenance(FenetreInterface fenetreDAppartenance);
+	FenetreInterface setFenetrePrincipale(FenetreInterface fenetrePrincipale);
+	BoutonInterface creerBouton(String typeBouton);
+	void supprimerBouton(BoutonInterface boutonASupprimer);
+	void fermetureFenetre(FenetreInterface fenetreAFermer);
 	FenetreInterface ouvertureFenetre(String typeFenetre);
 	void envoiDInformationControleur();
 	
