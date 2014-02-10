013015605eaa63566ed71b7f95f60be7216fda37
zaccharieramzi <zaccharie.ramzi@gmail.com>
2014-02-10 09:34:29 +0100

    Ajout de certaines méthodes à l'interface BoutonInterface

--- src/GUI/BoutonInterface.java
+++ src/GUI/BoutonInterface.java
@@ -1,5 +1,10 @@
 package GUI;
 
 public interface BoutonInterface {
+	
+	String setText(String titreDuBouton);
+	FenetreInterface createFenetre(String titreDeLaFenetre);
+	void fermerFenetre(FenetreInterface fenetre);
+	
 
 }
