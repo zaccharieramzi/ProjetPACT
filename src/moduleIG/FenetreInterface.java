6148a0efe32167565ca34f330ed5226f581ef12e
Ndeye Seynabou DIOP <ndeye-seynabou.diop@telecom-paristech.fr>
2014-02-18 19:06:37 +0100

    creation des classes pour chaque fenêtre à créer, ajout de signature de méthodes dans  FenêtreInterface

--- src/moduleIG/FenetreInterface.java
+++ src/moduleIG/FenetreInterface.java
@@ -1,5 +1,10 @@
 package moduleIG;
 
-public interface FenetreInterface {
+import javax.swing.JFrame;
 
+public interface FenetreInterface {
+	public JFrame createWindow(String nameOfWindow);
+	public void setWindow(int locationOfButton);
+	public void closeWindow(JFrame windowToClose); //automatiquement
+	public void openWindow(JFrame WindowToSetVisible);
 }
