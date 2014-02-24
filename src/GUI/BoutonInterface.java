128e91b4df1f810e79693b70006027851b38250e
zaccharieramzi <zaccharie.ramzi@gmail.com>
2014-02-24 11:51:03 +0100

    Modification interface bouton pour coller à ce que l'on avait dit la dernière fois

--- src/GUI/BoutonInterface.java
+++ src/GUI/BoutonInterface.java
@@ -3,11 +3,7 @@ package GUI;
 public interface BoutonInterface {
 	
 	String setText(String titreDuBouton);
-	FenetreInterface createFenetre(String titreDeLaFenetre);
-	void fermerFenetre(FenetreInterface fenetre);
-	void mettreFenetrePrincipaleEnRetrait(FenetreInterface fenetrePrincipale);
-	String envoyerMessageAFenetrePrincipale(String message);
-	void passerDecor(String direction);
-	
+	FenetreInterface setFenetre(FenetreInterface fenetre);
+	void isClicked(FenetreInterface fenetre);	
 
 }
