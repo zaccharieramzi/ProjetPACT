03a53eac447ad13d52ec533a219b6151bc974ae3
adil hihi <hihi.adil@hotmail.fr>
2014-03-03 16:23:46 +0100

    Avancée dans le plan B

--- src/pact/ui/layout/FentreAffichageDemo.java
+++ src/pact/ui/layout/FentreAffichageDemo.java
@@ -1,26 +1,13 @@
 package moduleIG;
+package pact.ui.layout;
 
-
-import javax.swing.JFrame;
-import Jeu.Manitou;
-
-import java.awt.Container;
-import javax.swing.JButton;
-import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
-
-	public class FenetreAffichage {
-		
-		private JFrame fenetre;
-		private Manitou manitou;
+	import java.awt.Component;
+	import java.awt.Container;
+	import javax.swing.BoxLayout;
+	import javax.swing.JButton;
+	import javax.swing.JFrame;
 	
-	
-	public FenetreAffichage()
-	{			
-	}
-	
+public class FentreAffichageDemo {
 	
 
 	/*
@@ -54,84 +41,47 @@ import java.awt.Dimension;
 	 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */ 
 
-
-
 	/*
-	 * BorderLayoutDemo.java
-	 *
+	 * BoxLayoutDemo.java requires no other files.
 	 */
 
 
-	    public static boolean RIGHT_TO_LEFT = false;
-	    
 	    public static void addComponentsToPane(Container pane) {
-	        
-	        if (!(pane.getLayout() instanceof BorderLayout)) {
-	            pane.add(new JLabel("Container doesn't use BorderLayout!"));
-	            return;
-	        }
-	        
-	        if (RIGHT_TO_LEFT) {
-	            pane.setComponentOrientation(
-	                    java.awt.ComponentOrientation.RIGHT_TO_LEFT);
-	        }
-	        
-	        JButton button = new JButton("Facile (PAGE_START)");
-	        pane.add(button, BorderLayout.PAGE_START);
-	        
-	        //Make the center component big, since that's the
-	        //typical usage of BorderLayout.
-	        button = new JButton("Normal (CENTER)");
-	        button.setPreferredSize(new Dimension(200, 100));
-	        pane.add(button, BorderLayout.CENTER);
-	        
-	        button = new JButton("Difficile (LINE_START)");
-	        pane.add(button, BorderLayout.LINE_START);
-	        
-	        button = new JButton("Expert (PAGE_END)");
-	        pane.add(button, BorderLayout.PAGE_END);
-	        
-	        button = new JButton("Jouer (LINE_END)");
-	        pane.add(button, BorderLayout.LINE_END);
+	        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
+	        addAButton("Facile", pane);
+	        addAButton("Normal", pane);
+	        addAButton("Difficile", pane);
+	        addAButton("Elevé", pane);
+	        addAButton("Jouer", pane);
 	    }
-	    
+
+	    private static void addAButton(String text, Container container) {
+	        JButton button = new JButton(text);
+	        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        container.add(button);
+	    }
+
 	    /**
 	     * Create the GUI and show it.  For thread safety,
 	     * this method should be invoked from the
-	     * event dispatch thread.
+	     * event-dispatching thread.
 	     */
 	    private static void createAndShowGUI() {
-	        
 	        //Create and set up the window.
-	        JFrame frame = new JFrame("BorderLayoutDemo");
+	        JFrame frame = new JFrame("BoxLayoutDemo");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	        //Set up the content pane.
 	        addComponentsToPane(frame.getContentPane());
-	        //Use the content pane's default BorderLayout. No need for
-	        //setLayout(new BorderLayout());
+
 	        //Display the window.
 	        frame.pack();
 	        frame.setVisible(true);
 	    }
-	    
+
 	    public static void main(String[] args) {
-	        /* Use an appropriate Look and Feel */
-	        try {
-	            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-	            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-	        } catch (UnsupportedLookAndFeelException ex) {
-	            ex.printStackTrace();
-	        } catch (IllegalAccessException ex) {
-	            ex.printStackTrace();
-	        } catch (InstantiationException ex) {
-	            ex.printStackTrace();
-	        } catch (ClassNotFoundException ex) {
-	            ex.printStackTrace();
-	        }
-	        /* Turn off metal's use bold fonts */
-	        UIManager.put("swing.boldMetal", Boolean.FALSE);
-	        
-	        //Schedule a job for the event dispatch thread:
+	        //Schedule a job for the event-dispatching thread:
 	        //creating and showing this application's GUI.
 	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	            public void run() {
@@ -140,4 +90,3 @@ import java.awt.Dimension;
 	        });
 	    }
 	}
-
