a2d09e99a17767b7557e4891a207113e6abf70a9
zaccharieramzi <zaccharie.ramzi@gmail.com>
2014-03-04 10:47:42 +0100

    Suppression des classes inutiles, ajout des méthodes getObject au controleur et modification du plan B

--- src/Jeu/Controleur.java
+++ src/Jeu/Controleur.java
@@ -5,6 +5,8 @@ void lancerJeu() throws InterruptedException;
 void setDirectionP1(int direction);
 void setDirectionP2(int direction);
 void setSpeed(int vitesse);
-
+Balle getBalle();
+Raquette getRaquetteP1();
+Raquette getRaquetteP2();
 
 }
---
+++ src/moduleIG/MainTestIG.java
@@ -0,0 +1,13 @@
+package moduleIG;
+
+public class MainTestIG {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FenetreAffichage menu = new FenetreAffichage(); 
+	}
+
+}
