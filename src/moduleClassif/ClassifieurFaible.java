9b63a7da3e36e3de255628ea0bdbf21bd54bea1f
Castex <jimmy.castex@telecom-paristech.fr>
2014-03-06 15:38:58 +0100

    modification du classifieur faible

--- src/moduleClassif/ClassifieurFaible.java
+++ src/moduleClassif/ClassifieurFaible.java
@@ -1,51 +1,91 @@
+
 package moduleClassif;
 
+
 public class ClassifieurFaible {
-	
-	static int p, d ; //represente le nombre de features et le nombre de composantes
-	private static boolean [] newlabels = new boolean[d];
-	
-	//represente la matrice des features les colonnes sont les features. 
-	//Je veux faire une concat�nation des colonnes
-	private Vector[] matrixOfFeatures = new Vector[p] ; 
-	
-	private double[] weight ; //represente les poids en entree
-	
-	public ClassifieurFaible(double[] weight, Vector[] matrixOfFeatures){
+
+	private int[] labels;
+	// private static final String String = null;
+	static int l, d; // représente le nombre de lignes et de colonnes
+	private static int[] newlabels = new int[l];
+	private double[][] matrixOfFeatures = new double[l][d]; // représente la matrice des features les
+															// colonnes sont les
+															// features. Je veux
+															// faire une
+															// concaténation des
+															// colonnes
+	private double[] weight; // représente les poids en entree
+	double[] errorI;
+
+	public ClassifieurFaible(double[] weight, double[][] matrixOfFeatures,
+			int[] labels) {
 		this.matrixOfFeatures = matrixOfFeatures;
 		this.weight = weight;
+		this.labels = labels;
+
+		int i, j, p, index_threshold;
+		String[] labelshape = null;
+		String labelshapep;
+		double Ep;
+		
+
+		for (j = 0; j < d; j++) {   //parcours les colonnes
+
+			double[] coord = null;
+
+			for (i = 0; i < l; i++) {  
+				coord[i] = matrixOfFeatures[i][j];
+			}
+
+			Vector vector = new Vector(coord, labels);
+
+			for (i = 0; i < l; i++) {    //parcours les lignes
+
+				vector.counter1(i);
+				vector.counter2(i);
+
+				if (vector.counter1(i) > vector.counter2(i)) {
+
+					for (p = 0; p < i; p++) {
+						newlabels[p] = -1;
+					}
+					;
+					for (p = i; p < l; p++) {
+						newlabels[p] = 1;
+					}
+					;
+					labelshape[i] = "right1";
+				} else {
+					for (p = 0; p < i; p++) {
+						newlabels[p] = 1;
+					}
+					;
+					for (p = i; p < l; p++) {
+						newlabels[p] = -1;
+					}
+					;
+					labelshape[i] = "left1";
+				}
+				
+				errorI[i] = vector.error(weight, newlabels, labels);
+				
+				if(i!=1 && errorI[i]<errorI[i-1]){
+					index_threshold = i;
+					Ep = errorI[i];
+					labelshapep = labelshape[i];
+				}
+					
+				
+				}
+			
+			
+		}
 	}
-	
-	public Vector[] getMatrixOfFeatures(){
+
+	public double[][] getMatrixOfFeatures() {
 		return matrixOfFeatures;
 	}
 	
 	
-	public double error( double[] weight, boolean[] newlabels, boolean[] labels){
-		int i;
-		double Err = 0;
-		for(i=0; i<d; i++){
-			if(newlabels[i]!=labels[i]) {
-				Err = Err + weight[i]/d;
-			}
-		}
-		return Err;
-		
-	} //pas sur de ca..
-	
-	public void setNewLabels(){
-		int i,j,k ;
-		for(j=0; j<p;j++){
-			for(i=0;i<d;i++){
-				counter1(i);
-				counter2(i);
-				//à finir
-			}
-		}
-	}
-	
-	
-	
-	
-}
 
+}
