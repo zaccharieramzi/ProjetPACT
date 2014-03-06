0a8a4d206168cd846a9cabcc3178c75b11320f92
Ndeye Seynabou DIOP <ndeye-seynabou.diop@telecom-paristech.fr>
2014-03-06 21:52:15 +0100

    creation d'une classe Affichage qui implémente ( à faire) les methodes setDeco et setRaquette utilisées par la classe  FenetreAffichage pour lui donner la deco et la raquette choisies par le joueur

---
+++ src/Affichage/Affichage.java
@@ -0,0 +1,28 @@
+package Affichage;
+
+import moduleIntegration.SetRaquetteDecoGuiAffichage;
+
+
+// cette classe contient des methodes qui vont être appelees par GUI pour lui donner le décor et la raquette choisis 
+
+
+public class Affichage implements SetRaquetteDecoGuiAffichage {
+	
+	//créer un constructeur permettant de créer un écran d'affichage (il est utilisé par ex dans la classe fenetre
+	//pour appeler ses methodes setDeco et setRaquette
+	
+	public Affichage(){
+		super();
+	}
+
+	public void setRaquette(int choixRaquette) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDeco(int choixDeco) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
