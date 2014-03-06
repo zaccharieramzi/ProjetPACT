24cd07e538d3cb123452c77fbc1248b46eb0d9a9
Castex <jimmy.castex@telecom-paristech.fr>
2014-03-06 15:39:47 +0100

    modification

--- src/moduleClassif/VectorInterface.java
+++ src/moduleClassif/VectorInterface.java
@@ -2,10 +2,10 @@ package moduleClassif;
 
 public interface VectorInterface{
 	
-	public boolean [] getLabels();
-	public void setLabels(boolean [] labels0);
-	public int [] getCoord();
-	public void setCoord(int [] coord0);
+	public int [] getLabels();
+	public void setLabels(int [] labels0);
+	public double [] getCoord();
+	public void setCoord(double [] coord0);
 	public  int counter2(int i);
 	public  int counter1(int i);
 	public  int value(); //renvoie le minimum de |counter1(i)-counter2(i)|
