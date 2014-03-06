63fe7ad8eceb2c76cf926b5fd2c823b1c261f5d1
Castex <jimmy.castex@telecom-paristech.fr>
2014-03-06 15:39:11 +0100

    modification de la classe vector

--- src/moduleClassif/Vector.java
+++ src/moduleClassif/Vector.java
@@ -4,29 +4,25 @@ public class Vector implements VectorInterface {
 	
 	static int l ;
 
-	private static boolean [] labels = new boolean[l];
-	private int [] coord = new int[l];
+	private static int [] labels = new int[l];
+	private double [] coord = new double[l];
 
-	public Vector(int [] coord0, boolean [] labels0){
+	public Vector(double [] coord0, int [] labels0){
 	
 		labels = labels0 ;
 		coord = coord0 ;
 	
 	}
 	@Override
-	public boolean [] getLabels(){
+	public int[] getLabels(){
 		return labels;
 	}
 	@Override
-	public void setLabels(boolean [] labels0){
-		labels = labels0 ;
-	}
-	@Override
-	public int [] getCoord(){
+	public double [] getCoord(){
 		return coord ;
 	}
 	@Override
-	public void setCoord(int [] coord0){
+	public void setCoord(double [] coord0){
 		coord = coord0 ;
 	}
 	@Override
@@ -35,7 +31,7 @@ public class Vector implements VectorInterface {
 		
 		for(int j=0; j<i; j++) {
 			
-			if (labels[j]==false) {
+			if (labels[j]==-1) {
 				counter1 ++ ;
 			}
 		}
@@ -47,19 +43,35 @@ public class Vector implements VectorInterface {
 		
 		for(int j=i; j<l; j++) {
 			
-			if (labels[j]==false) {
+			if (labels[j]==-1) {
 				counter2 ++ ;
 			}
 		}
 		return counter2;
 	}
+	
+
+	public double error( double[] weight, int[] newlabels, int[] labels){
+		int i;
+		double Err = 0;
+		for(i=0; i<l; i++){
+			if(newlabels[i]!=labels[i]) {
+				Err = Err + weight[i]/l;
+			}
+		}
+		return Err;
+		
+	}
+	@Override
+	public void setLabels(int[] labels0) {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
 	public int value() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-
+	} 
 }
 
 
