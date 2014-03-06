fcdcb43f74706943125beb23f6bda88d2f3ae5fe
Ndeye Seynabou DIOP <ndeye-seynabou.diop@telecom-paristech.fr>
2014-03-06 18:53:34 +0100

    cet interface permettra implémenter dans jeu.manitou déclare les methodes utilisées par le classifieur pour envoyer au controleur les directions(gauche/droite)

---
+++ src/moduleIntegration/SetDirectionInterfaceClassifCon.java
@@ -0,0 +1,11 @@
+package moduleIntegration;
+
+
+/// cet interface permettra implémenter dans jeu.manitou déclare les methodes utilisées par le classifieur 
+
+///pour envoyer au controleur les directions(gauche/droite)
+
+public interface SetDirectionInterfaceClassifCon {
+	void setDirectionP1(int direction);
+	void setDirectionP2(int direction);
+}
