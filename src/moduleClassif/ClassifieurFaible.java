3178607412c44f03bdf77b624e0652d8db3e2845
jimousse <jim.castex@gmail.com>
2014-03-17 14:51:27 +0100

    Update ClassifieurFaible.java

--- src/moduleClassif/ClassifieurFaible.java
+++ src/moduleClassif/ClassifieurFaible.java
@@ -1,4 +1,3 @@
-
 package moduleClassif;
 
 
@@ -6,7 +5,7 @@ public class ClassifieurFaible {
 
 	private int[] labels;
 	// private static final String String = null;
-	static int l, d; // représente le nombre de lignes et de colonnes
+	static int l = 3, d = 3; // représente le nombre de lignes et de colonnes
 	private static int[] newlabels = new int[l];
 	private double[][] matrixOfFeatures = new double[l][d]; // représente la matrice des features les
 															// colonnes sont les
@@ -16,19 +15,23 @@ public class ClassifieurFaible {
 															// colonnes
 	private double[] weight; // représente les poids en entree
 	double[] errorI;
-
+	private double error_best;
+	private String labelshape_best;  //labelshape lié au seuil du meilleur feature
+	private int index_threshold_best; //indice du seuil liée au meilleur feature
+	
 	public ClassifieurFaible(double[] weight, double[][] matrixOfFeatures,
 			int[] labels) {
 		this.matrixOfFeatures = matrixOfFeatures;
 		this.weight = weight;
 		this.labels = labels;
 
-		int i, j, p, index_threshold;
+		int i, j, p;
 		String[] labelshape = null;
-		String labelshapep;
-		double Ep;
+		int[] index_threshold_feature = null;
+		double[] error_feature = null;
+		String[] labelshape_feature = null;
 		
-
+		
 		for (j = 0; j < d; j++) {   //parcours les colonnes
 
 			double[] coord = null;
@@ -66,48 +69,77 @@ public class ClassifieurFaible {
 					;
 					labelshape[i] = "left1";
 				}
-<<<<<<< HEAD
-				
-				errorI[i] = vector.error(weight, newlabels, labels);
-				
-				if(i!=1 && errorI[i]<errorI[i-1]){
-					index_threshold = i;
-					Ep = errorI[i];
-					labelshapep = labelshape[i];
-=======
+
 
 				errorI[i] = vector.error(weight, newlabels, labels);
 
-				if (errorI[i+1] < errorI[i]) {
+				if (errorI[i] < errorI[i-1]) {
+				
 					index_threshold_feature[j] = i; // renvoie l'indice du seuil
 													// correspondant à l'erreur
 													// minimum et le stocke dans
 													// le taleau
 													// index_threshold_feature
+					
 					error_feature[j] = errorI[i]; // renvoie l'erreur minimum de
 													// la ligne i et le stocke
 													// dans le tableau
 													// Error_feature[p]
+				
 					labelshape_feature[j] = labelshape[i]; // renvoie le
 															// labelshape
 															// associé et le
 															// stocke dans le
 															// tableau
 															// labelfeature[]
->>>>>>> parent of 5173d0d... rajout d'une virgule
+
 				}
-					
+
+
+				}
+			
+			error_best = error_feature[0];
+			setLabelshape_best(labelshape_feature[0]);
+			index_threshold_best = index_threshold_feature[0];
+			
+			if(error_feature[j]<error_feature[j-1]) {
 				
-				}
-			
-			
+				
+				error_best = error_feature[j];
+				setLabelshape_best(labelshape_feature[j]);
+				index_threshold_best = index_threshold_feature[j];
+				
+			}
+
+
+			}
 		}
+
+	public double getError_best() {
+		return error_best;
 	}
 
-	public double[][] getMatrixOfFeatures() {
-		return matrixOfFeatures;
+	public void setError_best(double error_best) {
+		this.error_best = error_best;
+	}
+
+	public int getIndex_threshold_best() {
+		return index_threshold_best;
+	}
+
+	public void setIndex_threshold_best(int index_threshold_best) {
+		this.index_threshold_best = index_threshold_best;
+	}
+
+	public String getLabelshape_best() {
+		return labelshape_best;
+	}
+
+	public void setLabelshape_best(String labelshape_best) {
+		this.labelshape_best = labelshape_best;
 	}
 	
-	
+
+
 
 }
